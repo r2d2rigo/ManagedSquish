473d22f7c63222c5485f0aaeab674c16387e7ca3
Rodrigo Diaz <r2d2rigo@users.noreply.github.com>
2012-08-05 19:44:16 +0000

    Created dev branch.

---
+++ ManagedSquish/SquishFlags.cs
@@ -0,0 +1,39 @@
+﻿// ManagedSquish - Copyright (c) 2011 Rodrigo 'r2d2rigo' Díaz
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+namespace ManagedSquish
+{
+    /// <summary>
+    /// Same flag values as specified in squish.h
+    /// Refer to libsquish's documentation for actual usage.
+    /// </summary>
+    public enum SquishFlags
+    {
+        Dxt1 = (1 << 0),
+        Dxt3 = (1 << 1),
+        Dxt5 = (1 << 2),
+        ColourClusterFit = (1 << 3),
+        ColourRangeFit = (1 << 4),
+        ColourMetricPerceptual = (1 << 5),
+        ColourMetricUniform = (1 << 6), 
+        WeightColourByAlpha = (1 << 7),
+        ColourIterativeClusterFit = (1 << 8)
+    };
+}
---
+++ ManagedSquish/SquishWrapper.cs
@@ -0,0 +1,98 @@
+﻿// ManagedSquish - Copyright (c) 2011 Rodrigo 'r2d2rigo' Díaz
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+using System;
+
+namespace ManagedSquish
+{
+    public class SquishWrapper
+    {
+        private static bool isx64;
+
+        static SquishWrapper()
+        {
+            isx64 = (IntPtr.Size == 8);
+        }
+
+        public static byte[] CompressImage(byte[] rgba, int width, int height, SquishFlags flags)
+        {
+            int finalSize = GetStorageRequirements(width, height, flags);
+            byte[] finalData = new byte[finalSize];
+
+            unsafe
+            {
+                fixed (byte* rgbaPointer = rgba)
+                {
+                    fixed (byte* dataPointer = finalData)
+                    {
+                        if (isx64)
+                        {
+                            Wrapperx64.CompressImage(rgbaPointer, width, height, dataPointer, (int)flags);
+                        }
+                        else
+                        {
+                            Wrapperx86.CompressImage(rgbaPointer, width, height, dataPointer, (int)flags);
+                        }
+                    }
+                }
+            }
+
+            return finalData;
+        }
+
+        public static byte[] DecompressImage(byte[] dxtBlocks, int width, int height, SquishFlags flags)
+        {
+            int finalSize = width * height * 4;
+            byte[] finalData = new byte[finalSize];
+
+            unsafe
+            {
+                fixed (byte* dxtPointer = dxtBlocks)
+                {
+                    fixed (byte* dataPointer = finalData)
+                    {
+                        if (isx64)
+                        {
+                            Wrapperx64.DecompressImage(dataPointer, width, height, dxtPointer, (int)flags);
+                        }
+                        else
+                        {
+                            Wrapperx86.DecompressImage(dataPointer, width, height, dxtPointer, (int)flags);
+                        }
+                    }
+                }
+            }
+
+            return finalData;
+        }
+
+        public static int GetStorageRequirements(int width, int height, SquishFlags flags)
+        {
+            if (isx64)
+            {
+                return Wrapperx64.GetStorageRequirements(width, height, (int)flags);
+            }
+            else
+            {
+                return Wrapperx86.GetStorageRequirements(width, height, (int)flags);
+            }
+        }
+    }
+}
